5731433538d959ae0e744e4f38578e0125949327
Diego Castro <dcastro405@unab.edu.co>
2025-03-06 11:18:41 -0500

    Primer Controlador

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('products.index');
+    }
+
+
+    public function create(){
+        echo "FORMULARIO PARA CREAR UN PRODUCTO";
+    }
+
+    public function show($name){
+        echo "PRODUCTO: $name";
+    }
+}
